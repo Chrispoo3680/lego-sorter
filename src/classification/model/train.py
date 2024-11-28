f0c592aad6c15287feccee3845d74fd64f89b8b4
Chrispoo3680 <christobran@gmail.com>
2024-11-28 18:29:23 +0100

    Update train.py

--- src/classification/model/train.py
+++ src/classification/model/train.py
@@ -211,6 +211,7 @@ manual_transform = v2.Compose(
         v2.TrivialAugmentWide(num_magnitude_bins=16),
         v2.RandomHorizontalFlip(p=0.5),
         v2.RandomResizedCrop(size=(224, 224), antialias=True),
+        v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         v2.ToTensor(),
     ]
 )
